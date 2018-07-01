8054ce8afb079c0fa347d29e18ad9a64e310a4fa
Gianluca Marcon <marcon.gluca@gmail.com>
2018-07-01 13:29:24 +0200

    fixed bug in music handler

--- musicplayerhandler.sh
+++ musicplayerhandler.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 if [[ -z $(pgrep -xU $UID ncmpcpp) ]]; then
-    xfce4-terminal -e ncmpcpp --role=ncmpcpp
-    # i3-msg "[window_role=ncmpcpp] floating enable"
+    echo "Starting ncmpcpp"
+    gnome-terminal --role=ncmpcpp -- ncmpcpp 
+    i3-msg "[window_role=ncmpcpp] move scratchpad"
+    i3-msg "[window_role=ncmpcpp] scratchpad show"
 else
-    RESULT=$(i3-msg "[window_role=ncmpcpp] scratchpad show")
+    RESULT=$(i3-msg "[window_role="ncmpcpp"] scratchpad show")
+    echo $RESULT
     if [[ "$RESULT" = *"false"* ]]; then
         # send to scratchpad
-        i3-msg "[window_role=ncmpcpp] move scratchpad"
+        i3-msg "[window_role="ncmpcpp"] move scratchpad"
     fi
 fi
