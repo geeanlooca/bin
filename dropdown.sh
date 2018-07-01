f3463f75d5fb7757b03db2d1974c415fbdfb8176
Gianluca Marcon <marcon.gluca@gmail.com>
2018-07-01 14:04:02 +0200

    added dropdown terminal script

---
+++ dropdown.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# check if there is a window with role=dropdown 
+RESULT=$(i3-msg -t get_tree | grep dropdown)
+
+if [ -z "$RESULT" ]; then
+    echo "No dropdown"
+    gnome-terminal --role=dropdown -- tmux
+    i3-msg "[window_role=dropdown] move scratchpad"
+    i3-msg "[window_role=dropdown] scratchpad show"
+else
+    echo "Dropdown"
+    RESULT=$(i3-msg "[window_role="dropdown"] scratchpad show")
+    echo $RESULT
+    if [[ "$RESULT" = *"false"* ]]; then
+        # send to scratchpad
+        i3-msg "[window_role="dropdown"] move scratchpad"
+    fi
+fi
