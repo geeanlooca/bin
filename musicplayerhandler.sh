84d07c67848d338b57868e2c45bafdd3aeb335bb
Gianluca <marcon.gluca@gmail.com>
2019-02-11 14:21:13 +0100

    updated gitignore

--- musicplayerhandler.sh
+++ musicplayerhandler.sh
@@ -2,12 +2,13 @@
 
 if [[ -z $(pgrep -xU $UID ncmpcpp) ]]; then
     echo "Starting ncmpcpp"
-    gnome-terminal --role=ncmpcpp -- ncmpcpp 
+    gnome-terminal --role=ncmpcpp -- ncmpcpp
     i3-msg "[window_role=ncmpcpp] move scratchpad"
     i3-msg "[window_role=ncmpcpp] scratchpad show"
+
 else
-    RESULT=$(i3-msg "[window_role="ncmpcpp"] scratchpad show")
-    echo $RESULT
+    RESULT=$(i3-msg "[window_role="ncmpcpp"] scratchpad show"
+)    
     if [[ "$RESULT" = *"false"* ]]; then
         # send to scratchpad
         i3-msg "[window_role="ncmpcpp"] move scratchpad"
