e922d75ba6c9d9158cdd03abc0009f226ebd7710
Gianluca <marcon.gluca@gmail.com>
2019-02-11 14:20:01 +0100

    short script for searching the content of the clipboard on google scholar. useful when reading papers

--- scholar-search.sh
+++ scholar-search.sh
@@ -3,8 +3,8 @@
 # perform a google scholar search on the content of the clipboard
 
 SEARCH=$(xclip -o)
-xclip -o
 echo $SEARCH
 URL="https://scholar.google.it/scholar?q=${SEARCH}"
-xdg-open "$URL"
+echo $URL
+firefox "$URL"
 
